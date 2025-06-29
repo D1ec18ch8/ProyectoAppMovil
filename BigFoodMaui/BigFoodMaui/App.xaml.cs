fde8e5296de5aa72bd02fe01bf7fa1a2d120d4df
D1ec18ch8 <jeffersonmoragaloria@gmail.com>
2025-06-29 01:24:43 -0600

    a

---
+++ BigFoodMaui/BigFoodMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BigFoodMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
